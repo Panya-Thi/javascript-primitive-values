bb70fb3d8dc61d1d498794984c248d5ac334d89b
Panya Thitimathanont <panya.thi@gmail.com>
2026-03-03 20:32:49 +0700

    feat: Assignment_Panya

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000
+let discoutPrice = productPrice*0.25
+let showDiscountPrice = `ส่วนลด ${discoutPrice} บาท`
+console.log(showDiscountPrice)
+let finalPrice = ` ราคาหลังหักส่วนลด ${productPrice + discoutPrice} บาท `
+console.log(finalPrice)
--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,10 @@
 // Exercise #6: Displaying message
 
 let totalParts = 13000;
+let total = "Total:"
+let unit = "Machine parts"
+let concatSentence = total + " " + totalParts +" "+unit
+console.log(concatSentence)
 
-let concatSentence;
-let literalSentence;
+let literalSentence = ` Total ${totalParts} Machine parts `
+console.log(literalSentence)
